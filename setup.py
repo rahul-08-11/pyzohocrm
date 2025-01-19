dc682cbdca66892b5d2fb6b8a6a629db0e037168
rahul-08-11 <grahulkumar4444@gmail.com>
2025-01-19 12:26:51 +0530

    v0.1.2 add json and body

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyzohocrm",  # Replace with your package name
-    version="0.1.1",
+    version="0.1.2",
     description="A Basic Zoho Crm utility Package with Token Management",
     author="Rahul Kumar",
     author_email="rahul.work.programming@gmail.com",
